7fd3b4d1152805d9ea9f4fea72f8771f072ee87c
KM_CM <send.nex@gmail.com>
2025-08-05 19:49:10 +0300

    Update HumanSWAT.lua

--- lua/entities/HumanSWAT.lua
+++ lua/entities/HumanSWAT.lua
@@ -18,10 +18,10 @@ list.Set( "NPC", "HumanSWAT", {
 		"Glock18,M16A4",
 		"Glock18,weapon_smg1",
 		"Glock18,weapon_shotgun",
-		"Glock18,M16A4,M249",
-		"Glock18,weapon_smg1,M249",
-		"Glock18,weapon_shotgun,M249",
-		"Glock18,M249"
+		"Glock18,M16A4,M249SAW",
+		"Glock18,weapon_smg1,M249SAW",
+		"Glock18,weapon_shotgun,M249SAW",
+		"Glock18,M249SAW"
 	}
 } )
 
@@ -37,4 +37,5 @@ function ENT:Initialize()
 	self:SetMaxHealth( 100 )
 	BaseClass.Initialize( self )
 end
+
 
