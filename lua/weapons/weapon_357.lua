974a5ae902bd9145b07e7c818efe519100ec53d9
KM_CM <send.nex@gmail.com>
2025-09-10 14:26:54 +0300

    Add files via upload

---
+++ lua/weapons/weapon_357.lua
@@ -0,0 +1,59 @@
+DEFINE_BASECLASS "BaseWeapon"
+
+SWEP.Category = "Pistols"
+SWEP.PrintName = "#weapon_357"
+if CLIENT then language.Add( "weapon_357", "Colt Python" ) end
+SWEP.Instructions = "Primary to shoot."
+SWEP.Purpose = "Colt Python."
+SWEP.ViewModel = Model "models/weapons/c_357.mdl"
+SWEP.UseHands = true
+SWEP.WorldModel = Model "models/weapons/w_357.mdl"
+SWEP.Primary.ClipSize = 6 //Duh
+SWEP.Primary.DefaultClip = 6
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "Pistol"
+SWEP.Primary_flSpreadX = .017
+SWEP.Primary_flSpreadY = .017
+SWEP.Primary_flDamage = 120
+SWEP.Primary_flDelay = .25
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Ammo = ""
+SWEP.Spawnable = true
+SWEP.Slot = 1
+SWEP.vViewModelAim = Vector( -4.62, 0, .67 )
+SWEP.ViewModelFOV = 54
+SWEP.Crosshair = "Revolver"
+
+sound.Add {
+	name = "ColyPython_Shot",
+	channel = CHAN_WEAPON,
+	level = 150,
+	pitch = { 90, 110 },
+	sound = "^ColtPythonShot.wav"
+}
+
+function SWEP:Initialize() self:SetHoldType "Revolver" end
+
+function SWEP:PrimaryAttack()
+	if !self:CanPrimaryAttack() then return end
+	local owner = self:GetOwner()
+	self:FireBullets {
+		Attacker = owner,
+		Src = owner:GetShootPos(),
+		Dir = owner:GetAimVector(),
+		Tracer = 1,
+		Spread = Vector( self.Primary_flSpreadX, self.Primary_flSpreadY ),
+		Damage = self.Primary_flDamage
+	}
+	self:ShootEffects()
+	owner:SetAnimation( PLAYER_ATTACK1 )
+	local ed = EffectData()
+	ed:SetEntity( self )
+	ed:SetAttachment( 1 )
+	ed:SetFlags( 1 )
+	util.Effect( "MuzzleFlash", ed )
+	self:EmitSound "ColyPython_Shot"
+	self:TakePrimaryAmmo( 1 )
+	self:SetNextPrimaryFire( CurTime() + self.Primary_flDelay )
+end
