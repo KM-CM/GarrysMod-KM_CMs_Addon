ad1d846623354726de061aa2d022fe1331186d77
KM_CM <send.nex@gmail.com>
2025-11-20 15:03:00 +0200

    Another stupid mistake, and removed the stupid comment

--- lua/entities/BaseActor/Script.lua
+++ lua/entities/BaseActor/Script.lua
@@ -1,9 +1,3 @@
-/*
-In My Definition of Map Making, "Scripting" Things is Basically Using The Input Output System to Do Anything.
-Do Not Confuse It with Writing Code ( Also Scripting, Just Different Context ).
-This File is Made for The Ability to Script What Actors Do.
-*/
-
 function ENT:CustomInput( Input, Param, Activator, Caller ) end
 
 local __INPUTS__ = {
@@ -82,7 +76,7 @@ local __KEY_VALUES__ = {
 		else self:SetNPCClass( v ) end
 	end
 }
-__KEY_VALUES__.additionalequipment = __KEY_VALUES__.weapon
+__KEY_VALUES__.additionalequipment = __KEY_VALUES__.cweapon
 function ENT:KeyValue( k, v )
 	local f = __KEY_VALUES__[ string.lower( k ) ]
 	if f then f( self, k, v ) return end
